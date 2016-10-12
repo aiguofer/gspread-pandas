d29fe189567370c00eaf857fa94f8c73ccc457cf
Diego Fernandez <difernan@redhat.com>
2016-10-11 21:27:26 -0400

    Add reqs and modify setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,8 @@ with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
     all_reqs = f.read().split('\n')
 
 install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
-dependency_links = [x.strip().replace('git+', '') for x in all_reqs if x.startswith('git+')]
+dependency_links = [x.strip().replace('git+', '') for x in all_reqs
+                    if x.startswith('git+')]
 
 setup(
     name='gspread-pandas',
@@ -23,18 +24,19 @@ setup(
     description='A package to easily open an instance of a Google spreadsheet and interact with worksheets through Pandas DataFrames.',
     long_description=long_description,
     url='https://github.com/aiguofer/gspread-pandas',
-    download_url='https://github.com/aiguofer/gspread-pandas/tarball/' + __version__,
+    download_url='https://github.com/aiguofer/gspread-pandas/tarball/' +
+    __version__,
     license='BSD',
     classifiers=[
-      'Development Status :: 3 - Alpha',
-      'Intended Audience :: Developers',
-      'Programming Language :: Python :: 3',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 2.7',
     ],
-    keywords='',
+    keywords='gspread pandas google spreadsheets',
     packages=find_packages(exclude=['docs', 'tests*']),
     include_package_data=True,
     author='Diego Fernandez',
     install_requires=install_requires,
     dependency_links=dependency_links,
-    author_email='aiguo.fernandez@gmail.com'
-)
+    author_email='aiguo.fernandez@gmail.com')
