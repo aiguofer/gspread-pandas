216909a83d6a6eb0219d6f0412bc065026a7ddb2
Diego Fernandez <aiguo.fernandez@gmail.com>
2016-12-10 00:23:50 -0500

    Improve version handling

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
-__version__ = '0.9'
+exec(open('gspread_pandas/_version.py').read())
 
 here = path.abspath(path.dirname(__file__))
 
