8db21f72b1e50ffe69c0370529198b31cb8d459f
Diego Fernandez <difernan@redhat.com>
2017-05-25 13:44:24 -0400

    Minor formatting change

--- gspread_pandas/client.py
+++ gspread_pandas/client.py
@@ -364,8 +364,11 @@ class Spread():
         end_cell = (start[ROW] - 1, 0)
 
         for val_chunks in chunks(vals, int(chunk_size)):
-            start_cell = (end_cell[ROW] + 1, start[COL])
-            end_cell = (min(start_cell[ROW] + chunk_rows - 1, start[ROW] + num_rows - 1), end[COL])
+            start_cell = (end_cell[ROW] + 1,
+                          start[COL])
+            end_cell = (min(start_cell[ROW] + chunk_rows - 1,
+                            start[ROW] + num_rows - 1),
+                        end[COL])
             yield start_cell, end_cell, val_chunks
 
     @_ensure_auth
