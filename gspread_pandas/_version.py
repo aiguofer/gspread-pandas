1e72d7f654640e8d5a3abe4d9f1390f0065c9a8d
Diego Fernandez <difernan@redhat.com>
2018-03-24 11:49:52 -0600

    Changes to use bumpversion

--- gspread_pandas/_version.py
+++ gspread_pandas/_version.py
@@ -1,2 +1,4 @@
-__version_info__ = (0, 15, 6)
-__version__ = '.'.join(map(str, __version_info__))
+# -*- coding: utf-8 -*-
+
+__version__ = '0.15.6'
+__version_info__ = tuple([int(x) for x in __version__.split('.')])
