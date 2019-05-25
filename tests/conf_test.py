c56ad9ee3788fd32d25de772c19e303f686eaa6d
Diego Fernandez <aiguo.fernandez@gmail.com>
2019-05-25 14:43:46 -0600

    Improve tests

---
+++ tests/conf_test.py
@@ -0,0 +1,153 @@
+import json
+import os
+
+import pytest
+from Crypto.PublicKey import RSA
+from oauth2client.client import OAuth2Credentials
+from oauth2client.service_account import ServiceAccountCredentials
+
+from gspread_pandas import conf, exceptions
+
+
+def make_config(tmpdir_factory, config):
+    f = tmpdir_factory.mktemp("conf").join("google_secret.json")
+    f.write(json.dumps(config))
+    return os.path.split(f)
+
+
+@pytest.fixture
+def sa_config(tmpdir_factory):
+    config = {
+        "type": "service_account",
+        "project_id": "",
+        "private_key_id": "",
+        "private_key": RSA.generate(2048).exportKey("PEM").decode(),
+        "client_email": "",
+        "client_id": "",
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": "",
+    }
+    return make_config(tmpdir_factory, config)
+
+
+@pytest.fixture
+def oauth_config(tmpdir_factory):
+    config = {
+        "installed": {
+            "client_id": "",
+            "project_id": "",
+            "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+            "token_uri": "https://oauth2.googleapis.com/token",
+            "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+            "client_secret": "",
+            "redirect_uris": ["urn:ietf:wg:oauth:2.0:oob", "http://localhost"],
+        }
+    }
+    return make_config(tmpdir_factory, config)
+
+
+def unset_env():
+    os.environ[conf.CONFIG_DIR_ENV_VAR] = ""
+
+
+@pytest.fixture
+def set_oauth_config(request, oauth_config):
+    os.environ[conf.CONFIG_DIR_ENV_VAR] = oauth_config[0]
+    request.addfinalizer(unset_env)
+
+
+@pytest.fixture
+def set_sa_config(request, sa_config):
+    os.environ[conf.CONFIG_DIR_ENV_VAR] = sa_config[0]
+    request.addfinalizer(unset_env)
+
+
+@pytest.fixture
+def make_creds(oauth_config, set_oauth_config):
+    creds = {
+        "access_token": "",
+        "client_id": "",
+        "client_secret": "",
+        "refresh_token": "",
+        "token_expiry": "2019-05-25T04:21:52Z",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "user_agent": None,
+        "revoke_uri": "https://oauth2.googleapis.com/revoke",
+        "id_token": {
+            "iss": "https://accounts.google.com",
+            "azp": "",
+            "aud": "",
+            "sub": "",
+            "email": "",
+            "email_verified": True,
+            "at_hash": "MoXn24dfJiPj1RnBRLtLng",
+            "iat": 1558754512,
+            "exp": 1558758112,
+        },
+        "id_token_jwt": "",
+        "token_response": {
+            "access_token": "",
+            "expires_in": 3600,
+            "refresh_token": "",
+            "scope": "",
+            "token_type": "Bearer",
+            "id_token": "",
+        },
+        "scopes": [
+            "https://spreadsheets.google.com/feeds",
+            "https://www.googleapis.com/auth/userinfo.email",
+            "https://www.googleapis.com/auth/drive",
+        ],
+        "token_info_uri": "https://oauth2.googleapis.com/tokeninfo",
+        "invalid": False,
+        "_class": "OAuth2Credentials",
+        "_module": "oauth2client.client",
+    }
+    creds_dir = os.path.join(oauth_config[0], "creds")
+    conf.ensure_path(creds_dir)
+
+    with open(os.path.join(creds_dir, "default"), "w") as f:
+        json.dump(creds, f)
+
+
+class Test_get_config:
+    def test_no_file(self):
+        with pytest.raises(IOError):
+            conf.get_config(file_name="this_file_doesnt_exist")
+
+    def test_with_oauth(self, oauth_config):
+        c = conf.get_config(*oauth_config)
+        assert isinstance(c, dict)
+        assert "creds_dir" in c
+        assert len(c) > 1
+
+    def test_with_sa(self, sa_config):
+        c = conf.get_config(*sa_config)
+        assert isinstance(c, dict)
+        assert "creds_dir" in c
+        assert len(c) > 1
+
+
+class Test_get_creds:
+    def test_service_account(self, set_sa_config):
+        creds = conf.get_creds()
+        assert isinstance(creds, ServiceAccountCredentials)
+
+    def test_oauth_no_key(self, set_oauth_config):
+        with pytest.raises(exceptions.ConfigException):
+            conf.get_creds(user=None)
+
+    def test_oauth_first_time(self, mocker, set_oauth_config):
+        mocker.patch.object(conf, "run_flow")
+        conf.get_creds()
+        assert os.path.exists
+        conf.run_flow.assert_called_once()
+
+    def test_oauth_default(self, make_creds):
+        assert isinstance(conf.get_creds(), OAuth2Credentials)
+
+    def test_bad_config(self):
+        with pytest.raises(exceptions.ConfigException):
+            conf.get_creds(config={"foo": "bar"})
--- tests/util_test.py
+++ tests/util_test.py
@@ -4,6 +4,9 @@ import pytest
 
 from gspread_pandas import util
 
+TEST = 0
+ANSWER = 1
+
 
 @pytest.fixture
 def df():
@@ -47,6 +50,20 @@ def df_multiheader_w_multiindex():
 
 
 @pytest.fixture
+def df_multiheader_blank_top():
+    data = [[1], [3]]
+    cols = pd.MultiIndex.from_tuples([("", "subcol1")])
+    return pd.DataFrame(data, columns=cols)
+
+
+@pytest.fixture
+def df_multiheader_blank_bottom():
+    data = [[1], [3]]
+    cols = pd.MultiIndex.from_tuples([("col1", "")])
+    return pd.DataFrame(data, columns=cols)
+
+
+@pytest.fixture
 def data_multiheader():
     data = [
         ["", "col1", "col1"],
@@ -58,74 +75,148 @@ def data_multiheader():
 
 
 @pytest.fixture
+def data_multiheader_top():
+    data = [
+        ["test_index", "col1", "col1"],
+        ["", "subcol1", "subcol2"],
+        [1, 2, 3],
+        [4, 5, 6],
+    ]
+    return data
+
+
+@pytest.fixture
 def data_empty():
     return [[]]
 
 
-def test_parse_sheet_index(df):
-    assert util.parse_sheet_index(df, 1).index.name == "col1"
+class Test_parse_sheet_index:
+    def test_normal(self, df):
+        assert util.parse_sheet_index(df, 1).index.name == "col1"
+
+    def test_noop(self, df):
+        assert util.parse_sheet_index(df, 0).index.name == "test_index"
+
+    def test_multiheader(self, df_multiheader):
+        assert util.parse_sheet_index(df_multiheader, 1).index.name == "subcol1"
+
+    def test_multiheader2(self, df_multiheader):
+        assert util.parse_sheet_index(df_multiheader, 2).index.name == "subcol2"
+
+    def test_multiheader_blank_top(self, df_multiheader_blank_top):
+        assert (
+            util.parse_sheet_index(df_multiheader_blank_top, 1).index.name == "subcol1"
+        )
+
+    def test_multiheader_blank_bottom(self, df_multiheader_blank_bottom):
+        assert (
+            util.parse_sheet_index(df_multiheader_blank_bottom, 1).index.name == "col1"
+        )
 
 
-def test_parse_sheet_index_noop(df):
-    assert util.parse_sheet_index(df, 0).index.name == "test_index"
+class Test_parse_df_col_names:
+    def test_empty_no_index(self, df_empty):
+        assert util.parse_df_col_names(df_empty, False) == [[]]
+
+    def test_normal_no_index(self, df):
+        assert util.parse_df_col_names(df, False) == [["col1", "col2"]]
+
+    def test_multiheader_no_index(self, df_multiheader):
+        expected = [["col1", "col1"], ["subcol1", "subcol2"]]
+        assert util.parse_df_col_names(df_multiheader, False) == expected
+
+    def test_multiheader_w_index(self, df_multiheader_w_index):
+        expected = [["", "col1", "col1"], ["test_index", "subcol1", "subcol2"]]
+        assert util.parse_df_col_names(df_multiheader_w_index, True) == expected
+
+    def test_multiheader_w_multiindex(self, df_multiheader_w_multiindex):
+        expected = [["", "", "col1", "col1"], ["l1", "l2", "subcol1", "subcol2"]]
+        assert util.parse_df_col_names(df_multiheader_w_multiindex, True, 2) == expected
 
 
-def test_parse_sheet_index_multiheader(df_multiheader):
-    """In a multi-header situation, it should use the lower heading as the index name"""
-    assert util.parse_sheet_index(df_multiheader, 1).index.name == "subcol1"
+class Test_parse_sheet_headers:
+    def test_empty(self, data_empty):
+        assert util.parse_sheet_headers(data_empty, 0) is None
+
+    def test_normal(self, data_multiheader):
+        expected = pd.Index(["", "col1", "col1"])
+        assert util.parse_sheet_headers(data_multiheader, 1).equals(expected)
+
+    def test_multiheader(self, data_multiheader):
+        """Note that 'test_index' should be shifted up"""
+        expected = pd.MultiIndex.from_arrays(
+            [["test_index", "col1", "col1"], ["", "subcol1", "subcol2"]]
+        )
+        assert util.parse_sheet_headers(data_multiheader, 2).equals(expected)
+
+    def test_multiheader3(self, data_multiheader):
+        """Note that 'test_index' and 1 should be shifted up"""
+        expected = pd.MultiIndex.from_arrays(
+            [["test_index", "col1", "col1"], [1, "subcol1", "subcol2"], ["", 2, 3]]
+        )
+        assert util.parse_sheet_headers(data_multiheader, 3).equals(expected)
 
 
-def test_parse_sheet_index_multiheader2(df_multiheader):
-    """In a multi-header situation, it should use the lower heading as the index name"""
-    assert util.parse_sheet_index(df_multiheader, 2).index.name == "subcol2"
+def test_chunks():
+    tests = [
+        (([], 1), []),
+        (([1], 1), [[1]]),
+        (([1, 2], 1), [[1], [2]]),
+        (([1, 2, 3], 2), [[1, 2], [3]]),
+    ]
+    for test in tests:
+        assert [chunk for chunk in util.chunks(*test[TEST])] == test[ANSWER]
 
 
-def test_parse_df_col_names_empty_no_index(df_empty):
-    assert util.parse_df_col_names(df_empty, False) == [[]]
+def test_get_cell_as_tuple():
+    tests = [("A1", (1, 1)), ((1, 1), (1, 1))]
+    for test in tests:
+        assert util.get_cell_as_tuple(test[TEST]) == test[ANSWER]
+
+    bad_tests = [
+        "This is a bad cell string",
+        (1.0, 1.0),
+        (1, 1, 1),
+        {"x": "y"},
+        10000000,
+    ]
+
+    for test in bad_tests:
+        with pytest.raises(TypeError):
+            util.get_cell_as_tuple(test)
 
 
-def test_parse_df_col_names_normal_no_index(df):
-    assert util.parse_df_col_names(df, False) == [["col1", "col2"]]
+class Test_create_filter_request:
+    pass
 
 
-def test_parse_df_col_names_multiheader_no_index(df_multiheader):
-    expected = [["col1", "col1"], ["subcol1", "subcol2"]]
-    assert util.parse_df_col_names(df_multiheader, False) == expected
+class Test_create_frozen_request:
+    pass
 
 
-def test_parse_df_col_names_multiheader_w_index(df_multiheader_w_index):
-    expected = [["", "col1", "col1"], ["test_index", "subcol1", "subcol2"]]
-    assert util.parse_df_col_names(df_multiheader_w_index, True) == expected
+class Test_create_merge_cells_request:
+    pass
 
 
-def test_parse_df_col_names_multiheader_w_multiindex(df_multiheader_w_multiindex):
-    expected = [["", "", "col1", "col1"], ["l1", "l2", "subcol1", "subcol2"]]
-    assert util.parse_df_col_names(df_multiheader_w_multiindex, True, 2) == expected
+class Test_create_unmerge_cells_request:
+    pass
 
 
-def test_parse_sheet_headers_empty(data_empty):
-    assert util.parse_sheet_headers(data_empty, 0) is None
+class Test_create_merge_headers_request:
+    pass
 
 
-def test_parse_sheet_headers_normal(data_multiheader):
-    expected = pd.Index(["", "col1", "col1"])
-    assert util.parse_sheet_headers(data_multiheader, 1).equals(expected)
+class Test_deprecate:
+    pass
 
 
-def test_parse_sheet_headers_multiheader(data_multiheader):
-    """Note that 'test_index' should be shifted up"""
-    expected = pd.MultiIndex.from_arrays(
-        [["test_index", "col1", "col1"], ["", "subcol1", "subcol2"]]
-    )
-    assert util.parse_sheet_headers(data_multiheader, 2).equals(expected)
+class Test_monkey_patch_request:
+    util.monkey_patch_request
+    pass
 
 
-def test_parse_sheet_headers_multiheader3(data_multiheader):
-    """Note that 'test_index' and 1 should be shifted up"""
-    expected = pd.MultiIndex.from_arrays(
-        [["test_index", "col1", "col1"], [1, "subcol1", "subcol2"], ["", 2, 3]]
-    )
-    assert util.parse_sheet_headers(data_multiheader, 3).equals(expected)
+class Test_get_col_merge_ranges:
+    pass
 
 
 def test_fillna(df):
@@ -159,4 +250,4 @@ def test_get_contiguous_ranges():
     ]
 
     for test in tests:
-        assert util.get_contiguous_ranges(test[0], 0, 100) == test[1]
+        assert util.get_contiguous_ranges(test[TEST], 0, len(test[0])) == test[ANSWER]
